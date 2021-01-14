c7537b80744238833ad735c99fd384e819347d75
gustavo.silva <gustavo.ferreira0821@gmail.com>
2021-01-14 16:30:54 -0300

    The room tag is being sent to a block on the card

--- AddRoomOnCards/AddRoomOnCards.ino
+++ AddRoomOnCards/AddRoomOnCards.ino
@@ -41,10 +41,10 @@ boolean firstTime = true;
 uint8_t successRead;    // Inteiro variável para manter se tivermos uma leitura bem-sucedida do Reader
 
 byte storedCard[4];   // Armazena uma ID lida da EEPROM
-byte blockContent[16]; //Armazena a tag que irá para o cartão
-byte readCard[16];   // Armazena a identificação digitalizada do módulo RFID
-int blockCard = 8;
-int block = "";
+byte blockcontent[16]; //Armazena a tag que irá para o cartão
+byte readCard[4];   // Armazena a identificação digitalizada do módulo RFID
+byte readbackblock[18];
+int blockCard = 9;
 
 // Crie a instância MFRC522.
 constexpr uint8_t RST_PIN = 9;     // Configurável, veja o layout típico de pinos acima
@@ -71,25 +71,24 @@ void setup() {
   mfrc522.PCD_Init();    // Inicializar Hardware MFRC522
   //Se você definir Antenna Gain to Max, aumentará a distância de leitura
   // mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;//keyByte is defined in the "MIFARE_Key" 'struct' definition in the .h file of the library
+  }
+
 }
-/* Verifique se o cartão mestre definido, se não permitir que o usuário escolha um cartão mestre
-  Isso também é útil para apenas redefinir a Master Card
-  Você pode manter outros registros EEPROM apenas escrever diferente de 143 para o endereço EEPROM 1
-  Endereço EEPROM 1 deve conter um número mágico que é '143'
-*/
 
 ///////////////////////////////////////// Main Loop ///////////////////////////////////
 void loop () {
-  do {
-    successRead = getID();            // define o sucesso Leia para 1 quando formos ler do leitor, caso contrário, 0
-    digitalWrite(blueLed, LED_ON);    // Visualize o cartão mestre precisa ser definido
-    delay(100);
-    digitalWrite(blueLed, LED_OFF);
-    delay(100);
-  }
-  while (!successRead);
   if (programMode) {
     if ( firstTime ) { //Quando o cartão lido da primeira vez é denovo
+      do {
+        successRead = getID_Room();            // define o sucesso Leia para 1 quando formos ler do leitor, caso contrário, 0
+        digitalWrite(blueLed, LED_ON);    // Visualize o cartão mestre precisa ser definido
+        delay(100);
+        digitalWrite(blueLed, LED_OFF);
+        delay(100);
+      }
+      while (!successRead);
       Serial.println(F("Room tag was detected"));
       Serial.println(F("Exiting Program Mode"));
       Serial.println(F("-----------------------------"));
@@ -97,27 +96,36 @@ void loop () {
       return;
     }
     else {
-      Serial.println(F("Adcionando tag cartão..."));
-      writeBlock(blockCard, blockContent);;//Adcionar a tag de uma sala a uma cartão // talvez o parâmetro seja outro.
-      Serial.println("-----------------------------");
-      Serial.println(F("Adcionado!"));
+      Serial.println(F("Adding tag to card..."));
+      do {
+        successRead = writeBlock(blockCard, blockcontent); // define o sucesso Leia para 1 quando formos ler do leitor, caso contrário, 0
+        digitalWrite(blueLed, LED_ON);    // Visualize o cartão mestre precisa ser definido
+        delay(100);
+        digitalWrite(blueLed, LED_OFF);
+        delay(100);
+        Serial.print("Test!");
+      }
+      while (!successRead);
+      Serial.print("Test3!");
       firstTime = true;
       programMode = true;
     }
   }
   else {
     if (firstTime) {    // Se é a primeira vez que o RFID está lendo um cartão. Endendesse que o mesmo é de uma sala.
-      programMode = true;
-      Serial.println(F("Olá tag da sala! irei gravar você!"));
-      for (uint8_t i = 0; i < 16; i++) {
-        blockContent[i] = readCard[i];
-        Serial.print(blockContent[i], HEX);
+      do {
+        successRead = getID_Room();            // define o sucesso Leia para 1 quando formos ler do leitor, caso contrário, 0
+        digitalWrite(blueLed, LED_ON);    // Visualize o cartão mestre precisa ser definido
+        delay(100);
+        digitalWrite(blueLed, LED_OFF);
+        delay(100);
       }
-      Serial.println();
-      Serial.print(F("Será o valor gravado no bloco "));
-      Serial.println(blockCard);
-      Serial.println(F("Esperando cartão para ser gravado..."));
+      while (!successRead);
+      programMode = true;
+      Serial.println(F("Hello Tag da sala! Eu estou com seu código!"));
+      Serial.println(F("Esperando um cartão para adcionar seu código!"));
       Serial.println(F("-----------------------------"));
+
       firstTime = false;
     }
     else {
@@ -126,8 +134,36 @@ void loop () {
   }
 }
 
+uint8_t getID_Room() {
+  // Getting ready for Reading PICCs
+  if ( ! mfrc522.PICC_IsNewCardPresent()) { //Se um novo PICC colocado no leitor RFID continuar
+    return 0;
+  }
+  if ( ! mfrc522.PICC_ReadCardSerial()) {   //Desde que um PICC colocado obtenha o Serial e continue
+    return 0;
+  }
+  /* Há PICCs Mifare que têm cuidado UID de 4 ou 7 bytes se você usar PICC de 7 bytes
+    Acho que devemos assumir todos os PICC como eles têm 4 byte UID
+    Até suportarmos PICCs de 7 bytes */
+  Serial.println(F("O ID do cartão da sala é: "));
+  for ( uint8_t i = 0; i < 4; i++) {  //
+    blockcontent[i] = mfrc522.uid.uidByte[i];
+    Serial.print(blockcontent[i], HEX);
+  }
+  Serial.println("");
+
+  mfrc522.PICC_HaltA(); // PARAR LEITURA
+  return 1;
+}
+
 int writeBlock(int blockNumber, byte arrayAddress[])
 {
+  if ( ! mfrc522.PICC_IsNewCardPresent()) { //Se um novo PICC colocado no leitor RFID continuar
+    return 0;
+  }
+  if ( ! mfrc522.PICC_ReadCardSerial()) {   //Desde que um PICC colocado obtenha o Serial e continue
+    return 0;
+  }
   //this makes sure that we only write into data blocks. Every 4th block is a trailer block for the access/security info.
   int largestModulo4Number = blockNumber / 4 * 4;
   int trailerBlock = largestModulo4Number + 3; //determine trailer block for the sector
@@ -161,6 +197,8 @@ int writeBlock(int blockNumber, byte arrayAddress[])
     return 4;//return "4" as error message
   }
   Serial.println("block was written");
+  mfrc522.PICC_HaltA(); // PARAR LEITURA
+  return 1;
 }
 
 /////////////////////////////////////////  Access Granted    ///////////////////////////////////
@@ -189,55 +227,6 @@ void denied() {
 }
 
 
-///////////////////////////////////////// Get PICC's UID ///////////////////////////////////
-uint8_t getID() {
-  // Getting ready for Reading PICCs
-  if ( ! mfrc522.PICC_IsNewCardPresent()) { //Se um novo PICC colocado no leitor RFID continuar
-    return 0;
-  }
-  if ( ! mfrc522.PICC_ReadCardSerial()) {   //Desde que um PICC colocado obtenha o Serial e continue
-    return 0;
-  }
-  /* Há PICCs Mifare que têm cuidado UID de 4 ou 7 bytes se você usar PICC de 7 bytes
-    Acho que devemos assumir todos os PICC como eles têm 4 byte UID
-    Até suportarmos PICCs de 7 bytes */
-  Serial.println(F("Scanned PICC's UID:"));
-  for ( uint8_t i = 0; i < 16; i++) {  //
-    readCard[i] = mfrc522.uid.uidByte[i];
-    Serial.print(readCard[i], HEX);
-  }
-  Serial.println("");
-  mfrc522.PICC_HaltA(); // PARAR LEITURA
-  return 1;
-
-}
-
-
-
-
-void ShowReaderDetails() {
-  // Obtenha a versão do software MFRC522
-  byte v = mfrc522.PCD_ReadRegister(mfrc522.VersionReg);
-  Serial.print(F("MFRC522 Software Version: 0x"));
-  Serial.print(v, HEX);
-  if (v == 0x91)
-    Serial.print(F(" = v1.0"));
-  else if (v == 0x92)
-    Serial.print(F(" = v2.0"));
-  else
-    Serial.print(F(" (unknown),probably a chinese clone?"));
-  Serial.println("");
-  // Quando 0x00 ou 0xFF é retornado, a comunicação provavelmente falhou
-  if ((v == 0x00) || (v == 0xFF)) {
-    Serial.println(F("WARNING: Communication failure, is the MFRC522 properly connected?"));
-    Serial.println(F("SYSTEM HALTED: Check connections."));
-    // Visualize o sistema é interrompido
-    digitalWrite(greenLed, LED_OFF);  //  Certifique-se de que o LED verde esteja apagado
-    digitalWrite(blueLed, LED_OFF);   //  Certifique-se de que o LED AZUL esteja apagado
-    digitalWrite(redLed, LED_ON);   // LIGAR LED VERMELHO
-    while (true); // não vá mais longe
-  }
-}
 
 ///////////////////////////////////////// Cycle Leds (Program Mode) ///////////////////////////////////
 void cycleLeds() {
@@ -268,86 +257,7 @@ void normalModeOn () {
   digitalWrite(greenLed, LED_OFF);
   digitalWrite(relay, HIGH);
 }
-
-//////////////////////////////////////// Read an ID from EEPROM //////////////////////////////
-void readID( uint8_t number ) {
-  uint8_t start = (number * 4 ) + 2;    // Descobrir a posição inicial
-  for ( uint8_t i = 0; i < 4; i++ ) {     // Loop 4 vezes para obter os 4 bytes
-    storedCard[i] = EEPROM.read(start + i);   // Atribuir valores lidos da EEPROM para o array
-  }
-}
-
-///////////////////////////////////////// Add ID to EEPROM   ///////////////////////////////////
-void writeID( byte a[] ) {
-  if ( !findID( a ) ) {     // Antes de escrevermos para a EEPROM, verifique se já vimos este cartão antes!
-    uint8_t num = EEPROM.read(0);     // Obter o numer de espaços usados, a posição 0 armazena o número de cartões de identificação
-    uint8_t start = ( num * 4 ) + 6;  // Descubra onde o próximo slot começa
-    num++;                // Incrementar o contador por um
-    EEPROM.write( 0, num );     // Escreva a nova contagem para o contador
-    for ( uint8_t j = 0; j < 4; j++ ) {   // Laço de 4 vezes
-      EEPROM.write( start + j, a[j] );  // Escreva os valores da matriz para EEPROM na posição correta
-    }
-    successWrite();
-    Serial.println(F("Succesfully added ID record to EEPROM"));
-    tone(buzzer, 500);
-    delay(300);
-    noTone(buzzer);
-    tone(buzzer, 3000);
-    delay(300);
-    noTone(buzzer);
-  }
-  else {
-    failedWrite();
-    Serial.println(F("Failed! There is something wrong with ID or bad EEPROM"));
-  }
-}
-
-///////////////////////////////////////// Check Bytes   ///////////////////////////////////
-boolean checkTwo ( byte a[], byte b[] ) {
-  if ( a[0] != 0 )      // Certifique-se de que há algo no array primeiro
-    match = true;       //Suponha que eles combinem primeiro
-  for ( uint8_t k = 0; k < 4; k++ ) {   //Laço de 4 vezes
-    if ( a[k] != b[k] )     // SE a! = B, em seguida, defina match = false, um falhará, todos falharão
-      match = false;
-  }
-  if ( match ) {      // Verifique se a correspondência ainda é verdadeira
-    return true;      // Retorno verdadeiro
-  }
-  else  {
-    return false;       //Retorna falso
-  }
-}
-
-///////////////////////////////////////// Find Slot   ///////////////////////////////////
-uint8_t findIDSLOT( byte find[] ) {
-  uint8_t count = EEPROM.read(0);       // Leia o primeiro Byte da EEPROM que
-  for ( uint8_t i = 1; i <= count; i++ ) {    //Loop uma vez para cada entrada da EEPROM
-    readID(i);                // Leia uma ID da EEPROM, ela é armazenada no cartão armazenado [4]
-    if ( checkTwo( find, storedCard ) ) {   // Verifique para ver se o cartão armazenado leu de EEPROM
-      //é o mesmo que o cartão de identificação find [] passado
-      return i;         // O número do slot do cartão
-      break;          // Pare de olhar, encontramos
-    }
-  }
-}
-
-///////////////////////////////////////// Find ID From EEPROM   ///////////////////////////////////
-boolean findID( byte find[] ) {
-  uint8_t count = EEPROM.read(0);     // Leia o primeiro Byte da EEPROM que
-  for ( uint8_t i = 1; i <= count; i++ ) {    // Loop uma vez para cada entrada da EEPROM
-    readID(i);          // Leia uma ID da EEPROM, ela é armazenada no cartão armazenado [4]
-    if ( checkTwo( find, storedCard ) ) {   // Verifique para ver se o cartão armazenado leu de EEPROM
-      return true;
-      break;  // Stop looking we found it
-    }
-    else {    // If not, return false
-    }
-  }
-  return false;
-}
-
-///////////////////////////////////////// Write Success to EEPROM   ///////////////////////////////////
-// Pisca o LED verde 3 vezes para indicar uma gravação bem-sucedida na EEPROM
+// Pisca o LED verde 3 vezes para indicar uma gravação bem-sucedida no cartão
 void successWrite() {
   digitalWrite(blueLed, LED_OFF);
   digitalWrite(redLed, LED_OFF);
@@ -401,36 +311,6 @@ void successDelete() {
   delay(200);
   digitalWrite(blueLed, LED_ON);
   delay(200);
-}
-
-////////////////////// Check readCard IF is masterCard   ///////////////////////////////////
-// Verifique se o ID passado é o cartão mestre de programação
-boolean isMaster() {
-  Serial.print("UID tag :");
-  String content = "";
-  byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++)
-  {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
-    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    content.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  Serial.println();
-  Serial.print("Message : ");
-  content.toUpperCase();
-
-  if (content.substring(1) == "3B 1F 89 03" or content.substring(1) == "76 D8 AD 1F") //change here the UID of the card/cards that you want to give access 76 D8 AD 1F
-  {
-    Serial.println("Authorized access");
-    Serial.println();
-    delay(1000);
-    return true;
-  } else   {
-    Serial.println(" Access denied");
-    return false;
-  }
-
 }
 
 bool monitorWipeButton(uint32_t interval) {
