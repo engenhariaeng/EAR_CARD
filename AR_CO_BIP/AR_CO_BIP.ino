ca7f4753de2322e036fc4f02e1147e61bbdc2d45
gustavo.silva <gustavo.ferreira0821@gmail.com>
2021-01-26 18:03:16 -0300

    Refatoration to V2.1

--- AR_CO_BIP/AR_CO_BIP.ino
+++ AR_CO_BIP/AR_CO_BIP.ino
@@ -32,71 +32,118 @@ constexpr uint8_t blueLed = 5;
 constexpr uint8_t relay = 3;     // DEFINIR PINO DO RELAY
 constexpr uint8_t wipeB = 4;     // DEFINIR PINO DO BOTÃO RESSET
 
-boolean match = false;          // inicializar correspondência de cartão com falso
-boolean programMode = false;  // inicializa o modo de programação para falso
-boolean replaceMaster = false;
+uint8_t successRead;
 
-uint8_t successRead;    // Inteiro variável para manter se tivermos uma leitura bem-sucedida do Reader
-
-byte storedCard[4];   // Armazena uma ID lida da EEPROM
-byte readCard[4];   // Armazena a identificação digitalizada do módulo RFID
-byte masterCard[4];   // Armazena o ID da placa principal lido na EEPROM
-
-String content = "";
-int block = 1;
-
-// Crie a instância MFRC522.
-constexpr uint8_t RST_PIN = 9;     // Configurável, veja o layout típico de pinos acima
-constexpr uint8_t SS_PIN = 10;     // Configurável, veja o layout típico de pinos acima
+constexpr uint8_t RST_PIN = 9;
+constexpr uint8_t SS_PIN = 10;
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key;
 ///////////////////////////////////////// Setup ///////////////////////////////////
 void setup() {
-  //Arduino Pin Configuration
   pinMode(redLed, OUTPUT);
   pinMode(greenLed, OUTPUT);
   pinMode(blueLed, OUTPUT);
-  pinMode(wipeB, INPUT_PULLUP);   //Habilite o resistor de pull up
-  pinMode(relay, OUTPUT);//Tenha cuidado como o circuito do relé se comporta enquanto reinicia ou liga e desliga o seu Arduino
-  digitalWrite(relay, LOW);   // Certifique-se de que a porta esteja trancada
-  digitalWrite(redLed, LED_OFF);  // Certifique-se de que o led está desligado
-  digitalWrite(greenLed, LED_OFF);  // Certifique-se de que o led está desligado
-  digitalWrite(blueLed, LED_OFF); // Certifique-se de que o led está desligado
-  //Configuração do Protocolo
-  Serial.begin(9600);  // Inicialize as comunicações seriais com o PC
-  SPI.begin();           // MFRC522 Hardware usa protocolo SPI
-  mfrc522.PCD_Init();    // Inicializar Hardware MFRC522
-  //Se você definir Antenna Gain to Max, aumentará a distância de leitura
-  // mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
+  pinMode(wipeB, INPUT_PULLUP);
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, LOW);
+  digitalWrite(redLed, LED_OFF);
+  digitalWrite(greenLed, LED_OFF);
+  digitalWrite(blueLed, LED_OFF);
 
-  Serial.println(F("Access Control Example v0.1"));   // Para fins de depuração
-  //ShowReaderDetails();  // Mostrar detalhes de PCD - MFRC522 Card Reader detalhes
-  //Código de limpeza - Se o botão (wipeB) for pressionado durante a execução da instalação (ligado), a EEPROM
-  Serial.println(F("Waiting for a card..."));
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+
+  Serial.println(F("SGA - Ar Condicionado 2.1"));
+  Serial.println(F("Esperando um cartão para ser lido..."));
+
   for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
 }
 
-
-///////////////////////////////////////// Main Loop ///////////////////////////////////
 void loop () {
-  if ( findID() ) { // Se não, veja se o cartão está na EEPROM
-    Serial.println(F("Welcome, You shall pass"));
-    granted(5000);         // MANTENHA O NIVEL AUTO POR 5000 ms
-    asm volatile("jmp 0");
+  do {
+    successRead = WaitingCard();
+  } while (successRead != 1);
+  Serial.println(F("Verificando.."));
+
+  while (verifyAccess() == true) {
+    Serial.println(F("Ligado!"));
+    delay(5000);
   }
-  else {      // Caso contrário, mostre que o ID não era válido
-    Serial.println(F("Hello"));
-    Serial.println(F("You shall not pass"));
-    denied();
-    
-  }
-  
+
+  Serial.println(F("Close!"));
+
+  asm volatile("jmp 0");
 }
+
+uint8_t WaitingCard() {
+  if ( ! mfrc522.PICC_IsNewCardPresent()) { //Se um novo PICC colocado no leitor RFID continuar
+    return 0;
+  }
+  if ( ! mfrc522.PICC_ReadCardSerial()) {   //Desde que um PICC colocado obtenha o Serial e continue
+    return 0;
+  }
+
+  return 1;
+}
+
+
+boolean verifyAccess() {
+  boolean finded = false;
+  byte buffer[18];
+  byte len = 18;
+  int block = 1;
+
+  while (finded == false) {
+    String content = "";
+    MFRC522::StatusCode status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
+    if (status != MFRC522::STATUS_OK) {
+      return false;
+    }
+
+    status = mfrc522.MIFARE_Read(block, buffer, &len);
+    if (status != MFRC522::STATUS_OK) {
+      return false;
+    }
+    Serial.println(F("Este é o bloco lido!"));
+    for (uint8_t i = 0; i < 4; i++)
+    {
+      Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+      Serial.print(buffer[i], HEX);
+      content.concat(String(buffer[i] < 0x10 ? " 0" : " "));
+      content.concat(String(buffer[i], HEX));
+    }
+    Serial.println("");
+    content.toUpperCase();
+
+    finded = isMatch(content);
+
+    block = block + 1;
+
+    if (block >= 63) {
+      finded = true;
+    }
+  }
+  block = 1;;
+
+  return finded;
+}
+
+boolean isMatch(String readTag) {
+  if (readTag.substring(1) == "86 05 56 1F" /*70*/ or
+      readTag.substring(1) == "76 D3 79 1F" /*HS*/ or
+      readTag.substring(1) == "76 D7 2C 1F" /*GERAL*/
+     ) {
+    return true;
+  }
+  return false;
+}
+
+
+
+
 /////////////////////////////////////////  Access Granted    ///////////////////////////////////
 void granted ( uint16_t setDelay) {
-  /* digitalWrite(blueLed, LED_OFF);   // Desligue o LED azul
-    digitalWrite(redLed, LED_OFF);  // Desligue o LED VERMELHO
-    digitalWrite(greenLed, LED_ON);   // Desligue o LED VERDE */
   digitalWrite(relay, HIGH);     // LIGAR O AR!
   delay(setDelay);          // NIVEL ALTO POR ALGUNS SEGUNDOS
   digitalWrite(relay, LOW);    // NIVEL BAIXO
@@ -127,18 +174,6 @@ void cycleLeds() {
   delay(100);
   noTone(buzzer);
   delay(1000);
-  /* digitalWrite(redLed, LED_OFF);
-    digitalWrite(greenLed, LED_ON);
-    digitalWrite(blueLed, LED_OFF);
-    delay(200);
-    digitalWrite(redLed, LED_OFF);
-    digitalWrite(greenLed, LED_OFF);
-    digitalWrite(blueLed, LED_ON);
-    delay(200);
-    digitalWrite(redLed, LED_ON);
-    digitalWrite(greenLed, LED_OFF);
-    digitalWrite(blueLed, LED_OFF);
-    delay(200); */
 }
 
 //////////////////////////////////////// Normal Mode Led  ///////////////////////////////////
@@ -147,79 +182,6 @@ void normalModeOn () {
   digitalWrite(redLed, LED_OFF);
   digitalWrite(greenLed, LED_OFF);
   digitalWrite(relay, LOW);
-}
-
-//////
-///////////////////////////////////////// Find ID From EEPROM   ///////////////////////////////////
-boolean findID() {
-  int block = 1;
-  boolean finded = false;
-  boolean okay = false;
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    okay = false;
-  }
-
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    okay = false;
-  }
-
-  while (finded == false) {
-    String content = "";
-    byte buffer[18];
-    byte len = 18;
-
-    byte status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
-    if (status != MFRC522::STATUS_OK) {
-      Serial.println(F("Authentication failed: "));
-      Serial.println(mfrc522.GetStatusCodeName(status));
-      finded = true;
-      okay = false;
-    }
-
-    status = mfrc522.MIFARE_Read(block, buffer, &len);
-    if (status != MFRC522::STATUS_OK) {
-      Serial.println(F("Reading failed: "));
-      Serial.println(mfrc522.GetStatusCodeName(status));
-      finded = true;
-      okay = false;
-    }
-    Serial.print(block);
-    Serial.print(" block is: ");
-    for (uint8_t i = 0; i < 4; i++)
-    {
-      Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-      Serial.print(buffer[i], HEX);
-      content.concat(String(buffer[i] < 0x10 ? " 0" : " "));
-      content.concat(String(buffer[i], HEX));
-    }
-    Serial.println("");
-    content.toUpperCase();
-
-
-    if (content.substring(1) == "D0 20 2C 25" /*SALA 62*/
-       ) //change here the UID of the card/cards that you want to give access 76 D8 AD 1F
-    {
-      Serial.println("Authorized access");
-      Serial.println();
-      finded = true;
-      okay = true;
-      block = 1;
-      granted(500);
-    } else {
-      if (block + 1 == 63) {
-        Serial.println("Not authorized access");
-        Serial.println();
-        delay(100);
-        finded = true;
-        okay = false;
-        block = 1;
-        denied();
-      } else {
-        block = block + 1;
-      }
-    }
-  }
-  return okay;
 }
 
 ///////////////////////////////////////// Write Success to EEPROM   ///////////////////////////////////
